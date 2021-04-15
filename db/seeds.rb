05444cd8de7db581608a5394aa684b318a5d8618
Shivang <daveshivang15@gmail.com>
2021-04-14 19:40:10 -0500

    Add sample models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create(username: "abc")
+u2 = User.create(username: "def")
+
+p1 = Post.create(title: "First post", user_id: u1.id)
+p2 = Post.create(title: "Second post", user_id: u2.id)
+p3 = Post.create(title: "Third post", user_id: u2.id)
