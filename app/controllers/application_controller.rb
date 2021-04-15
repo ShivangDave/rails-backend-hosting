f52de60d3ed09fe461b15430a76733982dafc2ef
Shivang <daveshivang15@gmail.com>
2021-04-14 19:54:46 -0500

    Add homepage message..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def home
+    render json: { msg: 'If you see this then deployment worked :)'}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'application#home'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
